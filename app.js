903fadc76280eb059dc4d849d51543a0f8c8429f
Flavia-20 <flaviamonique38@gmail.com>
2024-07-08 18:47:23 -0300

    obterNumeroAleatorio
    função de sortear

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`até o número: ${ate}`);
+    let sorteados = [];
+    let numero;
 
+    for (let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+        
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+    let resultados = document.getElementById('resultado');
+    resultados.innerHTML = ` <label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
 }
 
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
