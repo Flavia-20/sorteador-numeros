a45d239c6bb50a767697a11950cb23aa0664eb72
Flavia-20 <flaviamonique38@gmail.com>
2024-07-08 16:49:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sorteador-numeros
+ Treinando a Lógica de programação com JavaScript com um sorteador de números
---
+++ app.js
@@ -0,0 +1,21 @@
+
+function sortear() {
+    let quantidade =parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`até o número: ${ate}`);
+
+}
+
+
+
+
+
+
+
+
+
+
